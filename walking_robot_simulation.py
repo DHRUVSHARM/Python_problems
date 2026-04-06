4380e3a0a49bdb20676c01459e48157467359b93
DHRUVSHARM <dhruv999ddrr@gmail.com>
2026-04-06 09:51:14 -0400

    ap_6_daily

--- walking_robot_simulation.py
+++ walking_robot_simulation.py
@@ -1,36 +1,71 @@
 from typing import List
 
+"""
+
+    robot on an XY plane
+    receive array of integers commands 
+
+    three possible instructions 
+    -2 : turn left 90 degrees
+    -1 : turn right 90 degrees
+
+    
+       0,4
+x = -y ,    x=y
+        
+        N
+    
+    W   R    E   
+        
+        S
+
+        
+
+        
+
+        x , y => -y , x -> left
+        x , y => y , -x  -> right
+
+        x + dx , y + dy
+        y + dx , -x + dy
+"""
 
 class Solution:
     def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+        obstacles = set((x , y) for x , y in obstacles)
+        x , y , result , dir = 0 , 0 , 0 , 'N' # initial dir 
 
-        ans = 0
-        obstacle_set = {(x, y) for x, y in obstacles}
-
-        # nesw moving right
-        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        curr_direction = 0
-        curr_position = (0, 0)
+        # command mapping , initial direction is N , we have diff, left dir, right dir 
+        command_map = {
+            'N' : [(0 , 1), 'W' , 'E'],
+            'S' : [(0 , -1), 'E' , 'W'],
+            'E' : [(1 , 0),  'N' , 'S'], 
+            'W' : [(-1 , 0), 'S' , 'N']
+        }
 
         for command in commands:
-            if command == -1:
-                # right
-                curr_direction = (curr_direction + 1) % 4
-            elif command == -2:
+            diff, left, right = command_map[dir]
+            dx , dy = diff
+            if command == -2:
                 # left
-                curr_direction = (curr_direction - 1 + 4) % 4
+                dir = left
+            elif command == -1:
+                # right
+                dir = right
             else:
-                # moving forward
-                for _ in range(1, command + 1):
-                    new_position = (
-                        curr_position[0] + directions[curr_direction][0],
-                        curr_position[1] + directions[curr_direction][1],
-                    )
-                    if new_position in obstacle_set:
-                        break
-                    else:
-                        curr_position = new_position
+                # atmost 9 walk since range is 1 to 9 
+                steps = command
+                while steps and (x + dx , y + dy) not in obstacles:
+                    
+                    x += dx
+                    y += dy
+                    steps -= 1
+                
+                result = max(result , x**2 + y**2)
+                
 
-                ans = max(ans, curr_position[0] ** 2 + curr_position[1] ** 2)
+        return result 
 
-        return ans
+
+
+
